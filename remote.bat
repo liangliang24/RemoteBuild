71d7fc0c34dc807e7f0124a4ab626206f0029c76
liangliang24 <62233176+liangliang24@users.noreply.github.com>
2025-06-16 21:01:28 +0800

    更合理的目录结构

--- remote.bat
+++ remote.bat
@@ -6,17 +6,19 @@ set USER=lltouchingfish
 set PASSWORD=lltouchingfish
 set HOST=192.168.48.128
 set PORT=22
-set LOCAL_DIR=%CD%\%PROJ_NAME%
-set REMOTE_DIR=./dev
+set LOCAL_DIR=%CD%
+set REMOTE_DIR=./dev/%PROJ_NAME%
 set PSCP=.\RemoteBuild\pscp.exe
 set PLINK=.\RemoteBuild\plink.exe
 
 %PLINK% %USER%@%HOST% -pw %PASSWORD% "mkdir %REMOTE_DIR%"
 
 :: 上传整个文件夹
-%PSCP% -r -P %PORT% -pw %PASSWORD% %LOCAL_DIR% %USER%@%HOST%:%REMOTE_DIR%
+%PSCP% -r -P %PORT% -pw %PASSWORD% %LOCAL_DIR%/src %USER%@%HOST%:%REMOTE_DIR%
+%PSCP% -r -P %PORT% -pw %PASSWORD% %LOCAL_DIR%/premake5 %USER%@%HOST%:%REMOTE_DIR%
+%PSCP% -r -P %PORT% -pw %PASSWORD% %LOCAL_DIR%/premake5.lua %USER%@%HOST%:%REMOTE_DIR%
 
 :: 编译并运行（可选，根据上传内容修改）
-%PLINK% -t -ssh %USER%@%HOST% -P %PORT% -pw %PASSWORD% "cd %REMOTE_DIR%/%PROJ_NAME% && premake5 gmake && make"
+%PLINK% -t -ssh %USER%@%HOST% -P %PORT% -pw %PASSWORD% "cd %REMOTE_DIR% && premake5 gmake && make"
 
 pause
